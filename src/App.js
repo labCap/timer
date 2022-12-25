eb01ed0f6e7cc3dd757228d3f6a77d1a41974064
labCap <banlazthosow@gmail.com>
2022-12-25 14:23:52 +0200

    moved the clock logic to Add

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
 import { Main } from "./components/Main/Main";
@@ -7,11 +7,26 @@ import { ThemeContext } from "./context/ThemeProvider";
 function App() {
   const { theme } = useContext(ThemeContext);
 
+  // logic for clock
+  let time = new Date().toLocaleTimeString();
+
+  const [ctime, setCtime] = useState(time);
+
+  const updateTime = () => {
+    let time = new Date().toLocaleTimeString();
+    setCtime(time);
+    setTimeout(updateTime, 1000);
+  };
+
+  setTimeout(updateTime, 1000);
+
+  //логіка для секундоміра
+
   return (
     <div className={`wrapper ${theme ? "dark" : "light"}`}>
       <Header />
 
-      <Main />
+      <Main ctime={ctime} />
 
       <Footer />
     </div>
