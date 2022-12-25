d6d01b919476400fd2547e5d48da792f87e59f1c
labCap <banlazthosow@gmail.com>
2022-12-25 17:29:47 +0200

    added block nav and link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { FaSun, FaMoon } from "react-icons/fa";
+import { Link } from "react-router-dom";
 import { ThemeContext } from "../../context/ThemeProvider";
 import { Button } from "../Button/Button";
 import { Logo } from "../Logo/Logo";
@@ -14,6 +15,17 @@ export const Header = () => {
       <div className="container">
         <div className="inner">
           <Logo />
+
+          <nav className="nav">
+            <ul className="nav__list">
+              <li className="nav__item">
+                <Link to={"/"}>Clock</Link>
+              </li>
+              <li className="nav__item">
+                <Link to={"/stopwatch"}>StopWatch</Link>
+              </li>
+            </ul>
+          </nav>
           <Button className={"theme-btn"} onClick={() => setTheme(!theme)}>
             {theme ? <FaSun /> : <FaMoon />}
           </Button>
