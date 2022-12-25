c935c4d1511f9a13033a4efc18fcd2275401f2bf
labCap <banlazthosow@gmail.com>
2022-12-25 11:11:58 +0200

    added logic to Clock

--- src/components/Clock/Clock.jsx
+++ src/components/Clock/Clock.jsx
@@ -2,5 +2,16 @@ import React, { useState } from "react";
 import "./Clock.scss";
 
 export const Clock = () => {
+  let time = new Date().toLocaleTimeString();
+
+  const [ctime, setCtime] = useState(time);
+
+  const updateTime = () => {
+    let time = new Date().toLocaleTimeString();
+    setCtime(time);
+  };
+
+  setInterval(updateTime, 1000);
+
   return <h1 className="clock">{ctime}</h1>;
 };
